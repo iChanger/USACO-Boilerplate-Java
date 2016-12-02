e992c4c2cf88406dbfb56edae05919304469df86
Ian <ichangschool@gmail.com>
2016-12-02 01:14:39 -0800

    Changed some bugs and made it easier to edit

--- idealJava.java
+++ idealJava.java
@@ -6,9 +6,10 @@ class idealJava {
         BufferedReader f = new BufferedReader(new FileReader("ADD IN FILE.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ADD IN FILE.out")));
         
-        //StringTokenizer st = new StringTokenizer(f.readLine()); //Add only if a specific line in your input has spaces between what you want to input
-        //int firstInt = st.nextToken();
-        
+        //StringTokenizer st = new StringTokenizer(f.readLine()); 
+        //Add only if a specific line in your input has spaces between what you want to input
+        //int firstInt = Integer.parseInt(st.nextToken());
+        //int secondInt = Integer.parseInt(st.nextToken());
         int i1 = Integer.parseInt(f.readLine());
         int[] someArray = new int[i1];
         
